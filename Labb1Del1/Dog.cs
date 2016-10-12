48fdda9a655a5074d7b20b938383f3c9ba2bf6a8
Marc Uddenfeldt <Marc Uddenfeldt>
2016-10-12 11:38:29 +0200

    Klasser och objekt

---
+++ Labb1Del1/Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb1Del1
+{
+    class Dog
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Breed { get; set; }
+        public string WriteDogs()
+        {
+            return string.Format("here are your dogs {0}, {1}, {2}", Name, Age, Breed);
+        }
+        
+    }
+}
---
+++ Labb1Del1/Runtime.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb1Del1
+{
+    class Runtime
+    {
+        public void Start()
+        {
+            Dog dog1 = new Dog
+            {
+                Name = "göran",
+                Age = 10,
+                Breed = "Labrador"
+            };
+            Dog dog2 = new Dog
+            {
+                Name = "spartacus",
+                Age = 2,
+                Breed = "Chihuahua"
+            };
+            Dog dog3 = new Dog
+            {
+                Name = "Kexet",
+                Age = 3,
+                Breed = "mastiff"
+            };
+            List<Dog> dogs = new List<Dog>()
+            {
+                dog1,
+                dog2,
+                dog3
+            };
+    }
+
+    }
+}
