3474f8e4185adc2c19de5e2005c6ac5cc259a4e5
Marc Uddenfeldt <marcuddenfeldt@hotmail.com>
2016-10-12 13:21:30 +0200

    har ändrat lite

--- Labb1Del1/Runtime.cs
+++ Labb1Del1/Runtime.cs
@@ -34,7 +34,42 @@ namespace Labb1Del1
                 dog2,
                 dog3
             };
-    }
+            foreach (var dog in dogs)
+            {
+                Console.WriteLine(dog.Name + " " + dog.Age + " " + dog.Breed);
+            }
+            bool loop = false;
+            do
+            {
+                loop = false;
+                Console.WriteLine("Welcome to our dogcollection");
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("1. Add new dog");
+                Console.WriteLine("2. Remove dog");
+                Console.WriteLine("3. Show info");
+                Console.WriteLine("4. Exit");
+                var input = Console.ReadKey();
+
+                switch (input)
+                {
+                    case 1:
+                        Console.WriteLine("Dogs Name");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Dogs Age");
+                        int Age = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Dogs breed");
+                        string breed = Console.ReadLine();
+
+                    default:
+                        break;
+                }
+                case ConsoleKey.Y:
+                Console.WriteLine(newDog.Introduction());
+
+                
+
+            } while (true);
+        }
 
     }
 }
