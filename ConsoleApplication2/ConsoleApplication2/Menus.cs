42de59d7d65c96b7efc120897b6da17e9787b9dd
Marc Uddenfeldt <marcuddenfeldt@hotmail.com>
2016-11-08 11:07:21 +0100

    Labb8

---
+++ ConsoleApplication2/ConsoleApplication2/Menus.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Menus
+    {
+
+        public static void TillAgnes()
+        {
+            Console.WriteLine("Till dig min älskade");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("1. Kärleksförklaring");
+            Console.WriteLine("2. Gå ur ditt alldeles egna program");
+        }
+
+        public static void KärleksFörklaring()
+        {
+            Console.Clear();
+            Console.WriteLine("Du är allt jag vill ha och allt jag någonsin önskar få, och jag är så otroligt glad att jag har fått dig");
+            Console.WriteLine("Agnes, du är verkligen helt otrolig, och jag önskar att ha dig i mina armar resten av mitt liv");
+            Console.WriteLine("för du betyder ALLT för mig, alltså verkligen allt!, jag hoppas att jag kan få vara din resten av mitt liv.");
+            Console.WriteLine("Du är min dröm, min framtid och mitt LIV");
+            Console.WriteLine("Jag hoppas göra dig stolt varenda dag tills vi dör! Älskar dig! Din Marc");
+           
+            
+        }
+    }
+}
---
+++ ConsoleApplication2/ConsoleApplication2/Runtime.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Runtime
+    {
+        public static void PrintMenus()
+        {
+            Menus.TillAgnes();
+
+            
+
+            var choice = Console.ReadKey(true).Key;
+            switch (choice)
+            {
+                case ConsoleKey.D1:
+                case ConsoleKey.NumPad1:
+
+                    Menus.KärleksFörklaring();
+                    Console.ReadLine();
+                    return;
+                    break;
+
+                case ConsoleKey.D2:
+                case ConsoleKey.NumPad2:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Välj antingen 1 eller 2 älskling <3");
+                    break;
+            }
+        }
+    }
+}
